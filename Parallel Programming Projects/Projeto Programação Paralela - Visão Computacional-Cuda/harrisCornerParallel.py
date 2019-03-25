2480fbf30eefcd626d7427442dabb8351065b9fd
welerson <welerson.a.melo@gmail.com>
2019-03-25 00:53:40 -0300

    atualizacao code harris paralelo versao 1-nayve

---
+++ Parallel Programming Projects/Projeto Programação Paralela - Visão Computacional-Cuda/harrisCornerParallel.py
@@ -0,0 +1,75 @@
+import numpy as np
+import math
+import imgLib 
+import sys
+import baselib
+import matplotlib
+
+#################### MAIN #################################
+fileName = sys.argv[1]
+#img = cv.imread(fileName, cv.IMREAD_UNCHANGED)
+img = baselib.imread(fileName)
+#imgG = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+imgG = baselib.rgb2gray(img)
+h, w = baselib.size(imgG)
+
+#Gerando Gaussian mask 3X3
+g3Mask = imgLib.gaussianMaskGenerator()
+
+###Gaussian Convolution CUDA###
+imgG = imgLib.convolveGPU(imgG, g3Mask)
+
+###SobelMask 5X5 ###
+s5MaskX = imgLib.sobelMaskGenerator()
+
+#### Aplicando Sobel Operator a partir da convolução ####
+Ix = imgLib.convolveGPU(imgG, s5MaskX)
+Iy = imgLib.convolveGPU(imgG, np.transpose(s5MaskX))
+
+#cv.imwrite('response Iy.png', Iy)
+#cv.imwrite('response Ix.png', Ix)
+
+###Produto element wise de matriz: ou seja C[i][j] = A[i][j]*B[i][j]###
+Ix2 = np.multiply(Ix,Ix)
+Iy2 = np.multiply(Iy,Iy)
+Ixy = np.multiply(Ix,Iy)
+
+
+#### LEMBRETE: TENTAR COLOCAR ESSAS TRES LINHAS PARA RODAR EM PARALELO E VER SE TEM ERRO NA GPU DE ACESSO AS POSICOes
+Ix2 = imgLib.convolveGPU(Ix2, g3Mask)
+Iy2 = imgLib.convolveGPU(Iy2, g3Mask)
+Ixy = imgLib.convolveGPU(Ixy, g3Mask)
+
+### response calc Paralelizar com CPU ###
+response = imgLib.calcResponse(Ix2, Iy2, Ixy, h, w)
+
+###normalizing###
+#response = cv.normalize(response, response, 0.0, 1000.0, cv.NORM_MINMAX, cv.CV_32FC1)
+rmax, rmin = response.max(), response.min()
+response = ((response - rmin)/(rmax-rmin)) * 1000
+
+###nonMaxima Paralelizar com CPU(talvez)###
+# Lista de keypoint sendo a uma tupla da posicao na imagem (i, j) onde i é a linha e j a coluna
+keyPointsList = imgLib.nonMaxima(response)
+
+###put KeyPoints in the image###
+r = int(max(h, w)*0.015)
+
+for point in keyPointsList:
+	#cv.circle(img, (point[1],point[0]) , r, (0,0,255), -1)
+	i, j = point
+	
+	for u in range(-1,2):
+		for v in range(-1,2):
+			x = min(max(j + v, 0), w-1)
+			y = min(max(i + u, 0), h-1)
+			img[y][x] = (255,0,0)
+
+#baselib.imshow(img)
+matplotlib.image.imsave(fileName+'Keypoints.png', img)
+#cv.imwrite('KeyPoints'+fileName, img)
+
+#Jogando coordenadas dos keypoins num txt
+with open('keyPoints'+fileName+'.txt', 'w') as f:
+    for item in keyPointsList:
+        f.write('('+str(item[0]) +', '+ str(item[1])+')'+ "\n")
--- Parallel Programming Projects/Projeto Programação Paralela - Visão Computacional-Cuda/imgLib.py
+++ Parallel Programming Projects/Projeto Programação Paralela - Visão Computacional-Cuda/imgLib.py
@@ -1,6 +1,15 @@
 import numpy as np
 import math
 import baselib
+
+from pycuda import driver, compiler, tools
+
+import pycuda.autoinit
+
+import pycuda.gpuarray as gpuarray
+
+from pycuda.reduction import ReductionKernel
+
 ################################################################
 def calcResponse(Ix2, Iy2, Ixy, h, w):
 	response = []
@@ -10,7 +19,7 @@ def calcResponse(Ix2, Iy2, Ixy, h, w):
 			det = (Ix2[i][j]*Iy2[i][j]) - (Ixy[i][j]*Ixy[i][j])
 			trace = Ix2[i][j] + Iy2[i][j]
 			val = det - (0.04 * (trace*trace))
-			aux.append(0 if val <= 1 else val)
+			aux.append(0 if val <= 0 else val)
 		response.append(aux)
 	
 	return np.asarray(response, np.float32)
@@ -39,7 +48,7 @@ def convolve(img, kernel):
 					g += (kernel[s][t] * img[y][x])
 			aux.append(g)
 		outAux.append(aux)
-
+	
 	return np.asarray(outAux, np.float32)
 
 ################## generating Sobel Mask #######################
@@ -48,11 +57,12 @@ def sobelMaskGenerator():
     a2 = np.asarray([1,1,2,1,1])
     a3 = np.asarray([0,0,0,0,0])
     
-    return [a1, a2, a3, -a2, -a1] 
+    return np.asarray([a1, a2, a3, -a2, -a1])
 
 ################################################################
 def gaussianMaskGenerator():
-	return [[1/16,2/16,1/16],[2/16,4/16,2/16],[1/16,2/16,1/16]]
+	d = 159;
+	return np.asarray([[2/d,4/d,5/d,4/d,2/d], [4/d,9/d,12/d,9/d,4/d], [5/d,12/d,15/d,12/d,5/d] ,[4/d,9/d,12/d,9/d,4/d] ,[2/d,4/d,5/d,4/d,2/d]])	
 
 ################################################################
 def nonMaxima(img):
@@ -89,3 +99,51 @@ def nonMaxima(img):
 				keyPoints.append((i, j))
 		
 	return keyPoints
+
+
+def convolveGPU(img, kernel):
+	
+	kernel_code_template = """
+	__global__ void matrixMult(float *img, float *B, float *C)
+	{
+		int a = 5, b = 5;
+		int a2 = a/2, b2 = b/2;
+		float g = 0;
+		int j = threadIdx.x;
+		int i = threadIdx.y;
+		int n = %(N)s, m =  %(M)s;
+		
+		for (int s = 0; s < a; s+=1){
+			for (int t = 0; t < b; t+=1) {
+				int x = j+t-a2;
+				int y = i+s-b2;
+				x = min(max(x,0), n-1);
+				y = min(max(y,0), m-1);
+				g += B[s*a + t] * img[y*n + x];
+			}
+		}
+		C[i*n + j] = g;
+	}
+	"""
+	
+	m, n = baselib.size(img)
+	
+	emp = [[0.0 for _ in range(n)] for _ in range(m)]
+	# Transferindo os arrays para o device.
+	img_gpu = gpuarray.to_gpu(np.asarray(img, np.float32)) 
+	b_gpu = gpuarray.to_gpu(np.asarray(kernel, np.float32))
+	c_gpu = gpuarray.empty(np.asarray(emp, np.float32))
+
+	kernel_code = kernel_code_template % {
+		'N': n,
+		'M': m
+		}
+	mod = compiler.SourceModule(kernel_code)
+	convolution = mod.get_function("convolution")
+	
+	matrixMult(img_gpu, b_gpu, c_gpu, block = (m, n, 1))
+	
+	return c_gpu.get()
+	
+	
+
